d3cfdb8851bbbd51a9cc2a3b045954628a830ec5
Hai Son <son18061993@gmail.com>
2026-04-20 18:13:33 +0700

    fix port for deploy

--- Index.js
+++ Index.js
@@ -69,6 +69,8 @@ app.get("/", (req, res) => {
   res.send("Chatbot đang chạy 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server chạy tại http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
